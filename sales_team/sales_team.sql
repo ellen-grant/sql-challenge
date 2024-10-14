3fcca424445fc8e1037a9481baab0d77617d07ee
Ellen Grant <ellenckgrant@gmail.com>
2024-10-13 23:27:20 -0500

    add sixth query

---
+++ sales_team/sales_team.sql
@@ -0,0 +1,5 @@
+SELECT employees.emp_no, employees.last_name, employees.first_name
+FROM dept_emp
+JOIN employees ON dept_emp.emp_no = employees.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
